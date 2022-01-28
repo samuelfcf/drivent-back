36ece8833035dd00e152acd7e3f499681bfbfd71
Luis Felipe <l_felipelizeu@hotmail.com>
2022-01-28 20:49:39 -0300

    Create a invalid user error

--- src/services/client/auth.ts
+++ src/services/client/auth.ts
@@ -1,6 +1,6 @@
 import jwt from "jsonwebtoken";
 
-import UnauthorizedError from "@/errors/Unauthorized";
+import InvalidUserError from "@/errors/InvalidUserError";
 import User from "@/entities/User";
 import Session from "@/entities/Session";
 
@@ -8,7 +8,7 @@ export async function signIn(email: string, password: string) {
   const user = await User.findByEmailAndPassword(email, password);
 
   if (!user) {
-    throw new UnauthorizedError();
+    throw new InvalidUserError();
   }
 
   const token = jwt.sign({
